5a8abef7c763ece60336a5b099c56ed4ccc6b163
kherrera <kenher822@gmail.com>
2019-11-05 17:35:12 -0600

    Agregando controlador people

---
+++ CrudMVCRazorFetch/Controllers/PeaopleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudMVCRazorFetch.Controllers
+{
+    public class PeaopleController : Controller
+    {
+        // GET: Peaople
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
