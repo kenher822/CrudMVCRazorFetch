83014293d4d0ddc9fc12c83c061d815eaaa8bd8f
kherrera <kenher822@gmail.com>
2019-11-05 21:04:05 -0600

    Modificanco controlador People, agregando vista, modelo para nuevo registro

--- CrudMVCRazorFetch/Controllers/PeopleController.cs
+++ CrudMVCRazorFetch/Controllers/PeopleController.cs
@@ -10,7 +10,7 @@ namespace CrudMVCRazorFetch.Controllers
 {
     public class PeopleController : Controller
     {
-        // GET: Peaople
+        // GET: People
         public ActionResult Index()
         {
             return View();
@@ -32,6 +32,32 @@ namespace CrudMVCRazorFetch.Controllers
                      }).ToList();
             }
             return View(lst);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(PeopleViewModel model)
+        {
+            try
+            {
+                using (CrudMVCRazorFetchEntities db = new CrudMVCRazorFetchEntities())
+                {
+                    var oPeople = new People();
+                    oPeople.name = model.Name;
+                    oPeople.age = model.Age;
+                    db.People.Add(oPeople);
+                    db.SaveChanges();
+                }
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
         }
     }
 }
